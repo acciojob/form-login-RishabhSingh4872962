14bb8366a62c37b25cf87ae7350b929ba5c97a2d
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-02-18 20:18:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+const firstname=document.getElementsByTagName("fname");
+const lastname=document.getElementsByTagName("lname");
 function getFormvalue() {
     //Write your code here
-
+	alert(`${firstname} ${lastname}`)
 }
